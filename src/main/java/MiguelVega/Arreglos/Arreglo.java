fb45338a357e5c8e7f0541556cc8b0b45e2d8804
miguelangelvegaquiroz <miguel.vega@jalasoft.com>
2019-05-28 23:25:08 -0400

    Working with for, while and for-each

--- src/main/java/MiguelVega/Arreglos/Arreglo.java
+++ src/main/java/MiguelVega/Arreglos/Arreglo.java
@@ -4,7 +4,8 @@ public class Arreglo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 0;
+		int i = 0;int j;
+		
 		String[] cad = new String [5];
 			
 		cad[0] = "cad1";
@@ -13,41 +14,84 @@ public class Arreglo {
 		cad[3] = "cad4";
 		cad[4] = "cad5";
 		
-// using for		
+		// using for		
 		for (i = 0; i<cad.length; i++)
 		{
 			System.out.println(cad[i]);	
 		}
 		
-// using for each
-		for (String j : cad)
+		// using for each
+		for (String a : cad)
 		{
-			System.out.println(j);	
+			System.out.println(a);	
 		}
 		
 		
-// using while		
+		// using while
+		i=0;
 		while (i<cad.length)
 		{
 			System.out.println(cad[i]);
 			i+=1;
 		}
 		
-		/*
-		String[] cadenas=new String[5];
-		cadena[0]="Hola";
-		cadena[0]="new String("Hola")";
-		cadena[0]="ABC";
-		cadena[0]="123";
-		cadena[0]="new String("1234")";
-		
-		for (i = 0; i<cad.length; i++)
-		{
 			
-			System.out.println(cad[i]);	
+/*
+ * HOMEWORK	
+ */
+		
+		String[] cadena=new String[5];
+		cadena[0]="Hola";
+		cadena[1]=new String("Hola");
+		cadena[2]="ABC";
+		cadena[3]="123";
+		cadena[4]=new String("1234");		
+	
+		// Using for
+		String temp;
+		
+		for (i = 0; i<cadena.length-1; i++)
+		{
+			temp=cadena[i];
+		
+			for (j = (i+1); j<cadena.length; j++)
+			{
+				if (temp.equals(cadena[j]))						
+				{				
+					System.out.println("La cadena que se repite usando 'for' es: \n"+"'"+cadena[j]+"'"+"\n en las posiciones: "+i+" y "+j);
+				}				
+			}				
 		}
 		
-		*/
+		// Using while
+		i=0;j=0;
+		while(i<cadena.length-1)
+		{j=i+1;
+			temp=cadena[i];			
+			while(j<cadena.length)
+			{
+				if (temp.equals(cadena[j]))						
+				{				
+					System.out.println("La cadena que se repite usando 'while' es: \n"+"'"+cadena[j]+"'"+"\n en las posiciones: "+i+" y "+j);
+				}	
+				j+=1;
+			}
+			i+=1;
+		}
+	
+		// Using for-each
+		i=0;j=0;				
+				for (String x : cadena)
+				{
+					temp=x;
+					j=i+1;
+					for ( String y : cadena)
+					{
+						if (y.equals(temp) && (x!=y))						
+						{				
+							System.out.println("La cadena que se repite usando 'for each' es: \n"+"'"+y+"'"+"\n en las posiciones: "+i+" y "+j);
+						}
+					}
+				}
 	}
-
 }
