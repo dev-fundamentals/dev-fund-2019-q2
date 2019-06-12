7251f13a704379c31300366c5b7886f01181daca
tumael <tumael.suscripciones@gmail.com>
2019-06-12 08:19:55 -0400

    Added other unit test for add method

--- src/main/java/JhasmanyQuiroz/classes/class07UnitTest/CalculatorTest.java
+++ src/main/java/JhasmanyQuiroz/classes/class07UnitTest/CalculatorTest.java
@@ -4,39 +4,63 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-  @Test
-  public void calculatorAddValues() throws Exception {
-    int actual = Calculator.add("1,2");
-    int expected = 3;
+    @Test
+    public void calculatorAddValues() throws Exception {
+        int actual = Calculator.add("1,2");
+        int expected = 3;
 
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
 
-  @Test(expected = Exception.class)
-   public void add_negativeNumber_resultException() throws Exception {
-     int actual = Calculator.addNumbers("-1,-5");
-   }
+    @Test(expected = Exception.class)
+    public void add_negativeNumber_resultException() throws Exception {
+        int actual = Calculator.addNumbers("-1,-5");
+    }
 
-   @Test(expected = Exception.class)
-   public void add_addNegativeNumber_resultErrorMessage() throws Exception {
-     try {
-       Calculator.add("1");
-     } catch(Exception e) {
-       String actual = e.getMessage();
-       assertEquals(actual, "Negative Numbers are: -1 and -5");
-     }
-   }
+    @Test(expected = Exception.class)
+    public void add_addNegativeNumber_resultErrorMessage() throws Exception {
+        try {
+            Calculator.add("1");
+        } catch(Exception e) {
+            String actual = e.getMessage();
+            assertEquals(actual, "Negative Numbers are: -1 and -5");
+        }
+    }
 
-   @Test
+    @Test
     public void add_addNegativeNumber_resultErrorMessage1() {
-       Calculator.addNumbers("-1,-5");
-       thrown.expect(IllegalArgumentException.class);
-       thrown.expectMessage("There are negative numbers: '-1,-5'");
-   }
+        Calculator.addNumbers("-1,-5");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("There are negative numbers: '-1,-5'");
+    }
+
+    @Test
+    public void add_addBigNumberLessThan1001_resultIgnored() {
+        int actualResult = Calculator.addNumbers("2,1001");
+        int expectedResult = 2;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void add_addNumbersWithOneDelimiter_result() {
+        int actualResult = Calculator.addNumbers("//[-]\n1-2-3");
+        int expectedResult = 6;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void add_addNumbersWithAnyDelimiter_result() {
+        int actualResult = Calculator.addNumbers("//[-][%]\n1-2%3");
+        int expectedResult = 6;
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
