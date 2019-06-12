d01265b0d5e710163e8e769f369b144751a3faf7
Rocio Encinas <rencinas@ecisolutions.com>
2019-06-11 22:57:41 -0400

    added the delimiter and multiple delimeter test

--- src/main/java/RocioEncinas/unitTests/tdd/Calculator.java
+++ src/main/java/RocioEncinas/unitTests/tdd/Calculator.java
@@ -1,23 +1,51 @@
 package main.java.RocioEncinas.unitTests.tdd;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
+	// returns the delimiters contained in the string
+	public static String getDelimiters(String numbers) {
+		String delimiters = "";
+		Matcher m = Pattern.compile("\\[([^]]*)\\]").matcher(numbers);
+        List<String> allDelimiters = new ArrayList<>();
+        while (m.find()) {
+        	allDelimiters.add(m.group().replace("[","").replace("]",""));
+        }
+        
+        for(String d : allDelimiters) {
+        	delimiters += (delimiters.length() > 0 ? "|" : "") + d;
+        }
+        
+        return delimiters;
+	}
+	
 	public static int add(String numbers) throws Exception {
 		String[] numbersArray;
 		String negativeNumbers = "";
+		String delimiter = "\n|,"; 
+		
 		int result = 0;
-
+		
 		if (numbers != null && numbers.length() > 0) {
-			numbersArray = numbers.split("\n|,");
-			//numbersArray = numbers.split("\n");
+			if (numbers.contains("[")) {
+				delimiter = getDelimiters(numbers);
+				numbers = numbers.substring(numbers.indexOf("\n") + 1, numbers.length());
+			}
+			
+			numbersArray = numbers.split(delimiter);
 
-		  for(String current : numbersArray) {
-			 int number = Integer.valueOf(current.replaceAll(" ",""));
-			 if (number < 0) {
-				 negativeNumbers += (negativeNumbers.length() > 0 ? "," : "") + number;
-			 }
-			 result += number > 1000 ? 0 : number;
-		  }
+			for(String current : numbersArray) {
+				int number = Integer.valueOf(current.replaceAll(" ",""));
+				if (number < 0) {
+					negativeNumbers += (negativeNumbers.length() > 0 ? "," : "") + number;
+				}
+				result += number > 1000 ? 0 : number;
+			}
 		}
+		
 		if (negativeNumbers.length() > 0) {
 			System.out.println(negativeNumbers);
 			throw new Exception("Negative numbers are: " + negativeNumbers);
