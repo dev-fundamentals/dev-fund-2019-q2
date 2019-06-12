14a96845883008d6d4fc30ce500f666259ccd34f
miguelangelvegaquiroz <miguel.vega@jalasoft.com>
2019-06-12 08:59:48 -0400

    add polymorphism

---
+++ src/main/java/MiguelVega/polymorphism/Shape.java
@@ -0,0 +1,18 @@
+package main.java.MiguelVega.polymorphism;
+
+public class Shape {
+
+	void print()
+	{
+		System.out.println("I am a shape");
+	}
+
+	void print(int posx, int posy)
+	{
+		System.out.println("I am a shape with cordinates: "+posx + posy);
+	}
+	
+	void draw() {				
+	}
+	
+}
---
+++ src/main/java/MiguelVega/polymorphism/Square.java
@@ -0,0 +1,15 @@
+package main.java.MiguelVega.polymorphism;
+
+public class Square extends Shape{
+
+	void print()
+	{		
+		System.out.println("I am a square");
+	}
+	
+	void draw() {
+		System.out.println("*****");
+		System.out.println("******");
+		System.out.println("******");		
+	}
+}
