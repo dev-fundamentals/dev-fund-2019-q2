0affea05a3bec00a6dd7890c7ea0406219e0b51e
Rocio Encinas <rencinas@ecisolutions.com>
2019-06-13 09:27:55 -0400

    updating test main class

--- src/main/java/RocioEncinas/practiceInheritancePolymorphism/Test.java
+++ src/main/java/RocioEncinas/practiceInheritancePolymorphism/Test.java
@@ -3,10 +3,10 @@ package main.java.RocioEncinas.practiceInheritancePolymorphism;
 public class Test {
 	public static void main(String[] args) {
 		Figure[] figures = {
-				// adding Queen objects
+				// adding 2 Queen objects
 				new Queen(0),
 				new Queen(1),
-				// adding Castle objects
+				// adding 4 Castle objects
 				new Castle(0),
 				new Castle(1),
 				new Castle(0),
