476e138c6a67ce2aed1cfe2f41070c4675b74400
miguelangelvegaquiroz <miguel.vega@jalasoft.com>
2019-06-14 09:27:08 -0400

    Add Figure UML and Figure.java

---
+++ src/main/java/MiguelVega/Figures/Figure.java
@@ -0,0 +1,27 @@
+package main.java.MiguelVega.Figures;
+
+public class Figure {
+
+  public String getTag()
+  {
+      return "";
+  }
+
+  public String getFigureType()
+  {
+      return "";
+  }
+  
+  public double area()
+  {
+      return 0;
+  }
+
+  public void drawTxt()
+  {      
+  }
+
+  public void printDescription()
+  {      
+  }
+}
